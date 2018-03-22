6e47cb4202665be4363fc83ba7ab822d93261080
Simon Gordon <simon.gordon@tui.co.uk>
2018-03-22 20:06:23 +0000

    Adding a few colour-related methods

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,100 @@
 'use strict';
-console.log( 'app started' );
+// console.log( 'app started' );
 
 var $cards = $( '#cards' );
 
-$cards.on('click', '.card__swatch', function( e ) {
-	e.preventDefault();
+$cards.on( 'click', '.card__swatch', function() {
 	var $this = $( this );
-	$this.toggleClass( 'hide-rgb' );
+	colours.switchMode( $this );
 });
+
+$( '#switch' ).on('change', '[name="switch_mode"]', function() {
+	var val = $( this ).val();
+	colours.switchAll( val );
+});
+
+
+var colours = (function( $ ) {
+
+	var modes = ['hex', 'rgb', 'hsl'];
+	var length = modes.length;
+
+	function getBrightness() {
+
+		var $swatches = $cards.find( '.card__swatch' );
+		$swatches.each(function(index, el) {
+			var $el = $( el );
+			var hsl = $el.next( '.inputs' )
+				.find( '[data-format="hsl"]' )
+				.val()
+				.replace( '%)', '' )
+				.split(',');
+			// $el.attr( 'brightness', hsl[2] );
+			if ( hsl[2] >= 85 ) {
+				$el.addClass( 'pale' );
+				// $el.attr( 'data-bright', true );
+			} else {
+				// $el.attr( 'data-bright', false );
+			}
+		});
+
+	}
+
+	function switchAll( val ) {
+
+		var $swatches = $cards.find( '.card__swatch' );
+		$swatches.each(function(index, el) {
+			$( el ).attr('data-colourmode', val);
+		});
+
+	}
+
+	function switchMode( $el ) {
+		var currentMode = $el.attr('data-colourmode');
+		var index = modes.indexOf(currentMode);
+		// console.log('length: ' + length);
+		// console.log('index: ' + index);
+		if ( index === (length - 1) ) {
+			// console.log('index matches last item in array');
+			$el.attr('data-colourmode', modes[0]);
+		} else {
+			// console.log('nope');
+			$el.attr('data-colourmode', modes[index + 1]);
+		}
+
+	}
+
+	return {
+		getBrightness: getBrightness,
+		switchAll: switchAll,
+		switchMode: switchMode
+	}
+
+})( jQuery );
 
 
 var view = (function( $ ) {
 
 	function render( data ) {
 
-		console.log( data.palettes );
+		// console.log( data.palettes );
 		var $colours = $( '#cards' );
 		var cardsTemplate = $( '#cardsTemplate' ).html();
 
+		/*
 		for (var i = 0; i < data.palettes.length; i++) {
 			console.log( data.palettes[i] );
 		}
+		*/
 
+		// Render mustache template
 		$colours.html( Mustache.render(
 			cardsTemplate,
 			{ data: data.palettes }
 		) );
+
+		// trigger function to identify pale colours
+		colours.getBrightness();
 
 	}
 
